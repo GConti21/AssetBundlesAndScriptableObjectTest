1289efb12f95a49e784d2706b9980e3d9bd9fd5b
Gian Conti <ie253conti21@gmail.com>
2016-06-17 16:53:56 -0400

    Fix gitignore and add SimpleAssetBundleDownloader
    The original .gitignore file seems to have some problems, so I changed.
    
    The SimpleAssetBundleDownloader just download an asset bundle of a
    server and replace an Image with a sprite downloaded in the bundle

---
+++ AB_SO_Test/Assets/Scripts/DataScripts/MyDataObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class to represent some generic data.
+/// </summary>
+[CreateAssetMenu(fileName = "MyDataObject")]
+public class MyDataObject : ScriptableObject {
+    
+    /// <summary>
+    /// A string generic field.
+    /// </summary>
+    public string stringField;
+
+    /// <summary>
+    /// A float generic field.
+    /// </summary>
+    public float floatField;
+
+    /// <summary>
+    /// A sprite generic field.
+    /// </summary>
+    public Sprite spriteField;
+
+}
---
+++ AB_SO_Test/Assets/Scripts/DataScripts/SimpleAssetBundleDownloader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+/// <summary>
+/// Simple asset bundle downloader.
+/// </summary>
+public class SimpleAssetBundleDownloader : MonoBehaviour {
+
+    /// <summary>
+    /// The asset bundle URL.
+    /// </summary>
+    public string assetBundleUrl;
+
+    /// <summary>
+    /// The name of the asset bundle.
+    /// </summary>
+    public string assetBundleName;
+
+    public Image testingImage;
+
+    void Start () {
+        StartCoroutine(DownloadAssetBundle());
+    }
+
+    /// <summary>
+    /// Downloads the asset bundle.
+    /// </summary>
+    IEnumerator DownloadAssetBundle() {
+        // Download the file from the URL. It will not be saved in the Cache
+        // Memory is freed from the web stream (www.Dispose() gets called implicitly)
+        using (WWW www = new WWW(assetBundleUrl)) {
+            yield return www;
+            if (www.error != null) {
+                throw new Exception("WWW download had an error:" + www.error);
+            }                
+            AssetBundle bundle = www.assetBundle;
+            testingImage.sprite = bundle.LoadAsset<Sprite>(assetBundleName);
+
+//            foreach (string s in bundle.GetAllAssetNames()) {
+//                print ("s = " + s);
+//            }
+        }
+    }
+
+}
